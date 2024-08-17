dad920c0b6d410f81d4dfaba0d31a532d0fb3711
= <=>
2024-08-17 17:10:06 +0700

    home page frontend

---
+++ tour-management/frontend/src/pages/Home.jsx
@@ -0,0 +1,100 @@
+import React from 'react'
+import '../styles/home.css'
+import { Container, Row, Col } from 'reactstrap'
+
+import heroImg from '../assets/images/hero-img01.jpg'
+import heroImg02 from '../assets/images/hero-img02.jpg'
+import heroVideo from '../assets/images/hero-video.mp4'
+import worldImg from '../assets/images/world.png'
+
+import Subtitle from '../shared/Subtitle'
+import SearchBar from './../shared/SearchBar'
+import FeaturedTourList from '../components/Featured-tours/FeaturedTourList'
+import MasonryImagesGallery from '../components/Image-gallery/MasonryImagesGallery'
+
+const Home = () => {
+  return (
+    <>
+
+    {/* banner */}
+    <section>
+      <Container>
+        <Row>
+          <Col lg='6'>
+            <div className='hero__content'>
+              <div className="hero__subtitle d-flex align-items-center">
+                <Subtitle subtitle={'Know Before You Go'}/>
+                  <img src={worldImg} alt="" />
+              </div>
+              <h1>Traveling opens the door to creating 
+                <span className='highlight'> memories</span>
+              </h1>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                Adipisci consequuntur vitae, quia vero, soluta laudantium 
+                commodi a quod ex reiciendis voluptas debitis ullam. 
+                Quam cum deserunt explicabo architecto inventore laborum.
+              </p>
+            </div>
+          </Col>
+
+          <Col lg='2'>
+            <div className='hero__img-box'>
+              <img src={heroImg} alt="" />
+            </div>
+          </Col>
+
+          <Col lg='2'>
+            <div className='hero__img-box mt-4'>
+              <video src={heroVideo} alt="" controls/>
+            </div>
+          </Col>
+
+          <Col lg='2'>
+            <div className='hero__img-box mt-5'>
+              <img src={heroImg02} alt="" />
+            </div>
+          </Col>
+
+          <SearchBar/>
+        </Row>
+      </Container>
+    </section>
+
+    {/* featured tour */}
+    <section>
+      <Container>
+        <Row>
+          <Col lg='12' className="mb-5">
+            <Subtitle subtitle={"Explore"}/>
+            <h2 className='featured__tour-title'>Our featured tours</h2>
+          </Col>
+          <FeaturedTourList/>
+        </Row>
+      </Container>
+      
+    </section>
+
+    {/* gallary */}
+    <section>
+      <Container>
+        <Row>
+          <Col lg='12'>
+            <Subtitle subtitle={'Gallery'}/>
+            <h2 className='gallery__title'>Visit our customer tour gallary
+      
+            </h2>
+          </Col>
+          <Col lg='12'>
+            <MasonryImagesGallery/>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+
+
+    </>
+  )
+}
+
+export default Home
---
+++ tour-management/frontend/src/router/Routers.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import {Routes, Route, Navigate} from 'react-router-dom'
+import Home from '../pages/Home'
+import Tours from '../pages/Tours'
+import TourDetails from '../pages/TourDetails'
+import Login from '../pages/Login'
+import Register from '../pages/Register'
+import SearchResultList from '../pages/SearchResultList'
+import About from '../pages/About'
+import Flights from '../pages/Flights'
+import Stays from '../pages/Stays'
+import Blog from '../pages/Blog'
+
+const Routers = () => {
+  return (
+    <Routes>
+        <Route path='/' element={<Navigate to="/home"/>} />
+        <Route path='/home' element={<Home/>} />
+        <Route path='/tours' element={<Tours/>} />
+        <Route path='/tours/:id' element={<TourDetails/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/register' element={<Register/>} />
+        <Route path='/tours/search' element={<SearchResultList/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/flights' element={<Flights/>} />
+        <Route path='/stays' element={<Stays/>} />
+        <Route path='/Blog' element={<Blog/>} />
+    </Routes>
+  )
+}
+
+export default Routers
