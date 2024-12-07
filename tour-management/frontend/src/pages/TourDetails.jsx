598bbb313c8892b4e58f5e73b6cb334053f31071
= <=>
2024-12-07 17:10:11 +0700

    change organize code

--- tour-management/frontend/src/pages/TourDetails.jsx
+++ tour-management/frontend/src/pages/TourDetails.jsx
@@ -1,17 +1,16 @@
-import React, { useEffect, useRef, useState, useContext } from 'react';
-import '../styles/tour-details.css';
-import { Container, Row, Col, Form, ListGroup } from 'reactstrap';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import calculateAvgRating from '../utils/avgRating';
+import { Col, Container, Form, ListGroup, Row } from 'reactstrap';
 import avatar from '../assets/images/avatar.jpg';
 import Booking from '../components/Booking/Booking';
-import useFetch from '../hooks/useFetch';
-import { BASE_URL } from '../utils/config';
 import { AuthContext } from '../context/AuthContext';
-import axios from 'axios';
-import { GoogleGenerativeAI } from "@google/generative-ai";
-import { ChatGroq } from "@langchain/groq";
+import useFetch from '../hooks/useFetch';
+import '../styles/tour-details.css';
+import calculateAvgRating from '../utils/avgRating';
+import { BASE_URL } from '../utils/config';
+// import { GoogleGenerativeAI } from "@google/generative-ai";
 import { HumanMessage } from "@langchain/core/messages";
+import { ChatGroq } from "@langchain/groq";
 
 const TourDetails = () => {
   const { id } = useParams();
