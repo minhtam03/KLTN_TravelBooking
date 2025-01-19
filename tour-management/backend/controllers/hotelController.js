b4a4c6a6a7d150dd1aad3d311f026578e65fe7ad
= <=>
2025-01-19 18:37:32 +0700

    add CRUD hotel

--- tour-management/backend/controllers/hotelController.js
+++ tour-management/backend/controllers/hotelController.js
@@ -10,7 +10,7 @@ export const createHotel = async (req, res) => {
             .status(200)    
             .json({
                 success: true,
-                message: "Successfully created",
+                message: "Successfully created hotel",
                 data: savedHotel,
             })
     } catch (error) {
@@ -18,15 +18,91 @@ export const createHotel = async (req, res) => {
             .status(500)
             .json({
                 success:false,
-                message: "Failed to create. Try again"
+                message: "Failed to create hotel"
             })
     }
 }
 
 // update hotel
+export const updateHotel = async (req, res) => {
+
+    const id = req.params.id
+
+    try {
+        const updatedHotel = await Hotel.findByIdAndUpdate(id, {
+            $set: req.body
+        }, {new: true})
+        res.status(200).json({
+            success: true,
+            message: "Successfully updated hotel",
+            data: updatedHotel
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Failed to update hotel",
+        })
+    }
+}
 
 // delete hotel
+export const deleteHotel = async (req, res) => {
+
+    const id = req.params.id
+
+    try {
+        await Hotel.findByIdAndDelete(id)
+        res.status(200).json({
+            success: true,
+            message: "Successfully deleted hotel",
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Failed to delete hotel",
+        })
+    }
+}
 
 // getSingle hotel
+export const getSingleHotel = async (req, res) => {
+    const id = req.params.id
+
+    try {
+        const hotel = await Hotel.findById(id)
+        res.status(200).json({
+            success: true,
+            message: "Successful get single hotel",
+            data: hotel,
+        })
+    } catch (error) {
+        res.status(404).json({
+            success: false,
+            message: "not found hotel",
+        })
+    }
+}
 
 // getAll hotel
+export const getAllHotel = async (req, res) => {
+
+    // for pagination
+    const page = parseInt(req.query.page)
+
+    try {
+        const hotels = await Hotel.find({})
+        .skip(page * 8).limit(8)
+
+        res.status(200).json({
+            success: true,
+            count: hotels.length,
+            message: "Successful get all hotel",
+            data: hotels
+        })
+    } catch (error) {
+        res.status(404).json({
+            success: false,
+            message: "not found hotel"
+        })
+    }
+}
--- tour-management/backend/routes/hotels.js
+++ tour-management/backend/routes/hotels.js
@@ -1,12 +1,25 @@
 import express from "express"
 
-import { createHotel } from "../controllers/hotelController.js";
+import { createHotel, updateHotel, deleteHotel, getSingleHotel, getAllHotel } from "../controllers/hotelController.js";
 
-// import { verifyAdmin } from "../utils/verifyToken.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-// create new tour
-router.post("/", createHotel)
+// create new hotel
+router.post("/", verifyAdmin, createHotel)
+
+// update hotel
+router.put("/:id", updateHotel)
+
+// delete hotel
+router.delete("/:id", deleteHotel)
+
+// get single hotel
+router.get("/:id", getSingleHotel)
+
+// get all hotel
+router.get("/", getAllHotel)
+
 
 export default router
